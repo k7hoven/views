808728d26a11a0c13a8c48538fd6612c4296ff31
Koos Zevenhoven <koos.zevenhoven@aalto.fi>
2017-10-10 21:37:16 +0300

    Version to 0.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="views",
-    version='0.2',
+    version='0.3',
     description="views",
     url='https://github.com/k7hoven/views',
     author='Koos Zevenhoven',
